b05e89876137a5834890b181bcd2aafe1b003a83
imTyrant <cd2235823998@gmail.com>
2017-06-01 07:48:00 +0800

    New

--- PipeExample/PipeTestClient/PipeTestClient/PipeTest_Client.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<Windows.h>
-
-HANDLE clientPipHandle;
-const wchar_t* pipeName = L"\\\\.\\pipe\\myPipe";
-
-void OpenNamedPipe() throw(DWORD);
-void WriteToPipe() throw(DWORD);
--- PipeExample/PipeTestServer/PipeTest/PipeTest_Server.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<Windows.h>
-
-HANDLE serverPipHandle;
-const wchar_t* pipeName = L"\\\\.\\pipe\\myPipe";
-
-void ServerCreatePIPE() throw(DWORD);
-void ReadFromPipe();
